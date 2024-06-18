bfd60c71e09ba075ae76204b0b4eb7414fe37b79
V-Kozintsev <v_kozintcev@mail.ru>
2024-06-18 16:01:10 +0300

    доделал форму

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const overlayHidden = document.querySelector(".overlay-hidden");
 const body = document.querySelector("body");
+const btnForm = document.querySelector(".btn-form");
 
 modalImg.addEventListener("click", function () {
   overlay.classList.toggle("overlay-hidden");
@@ -17,4 +18,9 @@ overlay.addEventListener("click", function () {
   modal.classList.toggle("modal-hidden");
   modal.style.display = "none";
   body.style.overflowY = "auto";
+});
+
+btnForm.addEventListener("click", () => {
+  overlay.classList.toggle("overlay-hidden");
+  modal.style.display = "none";
 });
